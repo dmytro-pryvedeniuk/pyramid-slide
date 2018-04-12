91ec24d7ef5f992d3c57e189ee35117778b798d5
Dmytro Pryvedeniuk <dmytro.pryvedeniuk@gmail.com>
2018-04-12 11:55:06 +0300

    Used jQuery

--- printPyramid.js
+++ printPyramid.js
@@ -1,22 +1,16 @@
-function printPyramid(containerSelector, height, char) {
-    function drawRowPart(n, divRow, className, char)
-    {
-        for (let i=0;i<n;i++)
-        {
-            let divBlock = document.createElement("div");
-            divBlock.className=className;
-            divBlock.innerHTML=char;
-            divRow.appendChild(divBlock);
-        };
+function printPyramid(container, height, char) {
+
+    function drawRowPart(n, divRow, className, char) {
+        for (let i = 0; i < n; i++) {
+            $(`<div class=${className}>${char}</div>`)
+                .appendTo(divRow);
+        }
     }
 
-    const container = document.body.querySelector(containerSelector);
-    for (var i=1;i<=height;i++){
-       
-        var divRow = document.createElement("div");
-        container.appendChild(divRow);
-        
-        drawRowPart(height-i, divRow, "block", "");
-        drawRowPart(i+1, divRow, "block", char);
+    for (var i = 1; i <= height; i++) {
+        container.append(`<div id="r${i}" class="rowblock"/>`);
+        let divRow = $(`#r${i}`);
+        drawRowPart(height - i, divRow, "block", "");
+        drawRowPart(i + 1, divRow, "block", char);
     }
-};
+}
